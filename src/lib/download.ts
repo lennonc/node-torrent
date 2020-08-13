32fcc0225fc30d6c9d3012b69942066ec1db8ee6
Lennon Chimbumu <lennonc@users.noreply.github.com>
2020-08-13 19:09:56 +0000

    Add non handshake message handlers

--- src/lib/download.ts
+++ src/lib/download.ts
@@ -4,27 +4,58 @@ import { Buffer } from 'buffer'
 import * as message from './message';
 import * as tracker from './tracker';
 import { Torrent, Peer } from '../types/torrent';
+import { Socket } from 'dgram';
 
 export default function (torrent: Torrent) {
+  const requested: any[]= [];
   tracker.getPeers(torrent, (peers: Peer[]) => {
-    peers.forEach((peer: Peer) => download(peer, torrent));
+    peers.forEach((peer: Peer) => download(peer, torrent, requested));
   });
 }
 
-function download(peer: Peer, torrent: Torrent) {
+function download(peer: Peer, torrent: Torrent, requested: any) {
   const socket = new net.Socket();
   socket.on('error', console.log);
   socket.connect(peer.port, peer.ip, () => {
     socket.write(message.buildHandshake(torrent));
   });
-  onWholeMsg(socket, (msg: any) => messageHandler(msg, socket));
+  onWholeMsg(socket, (msg: any) => messageHandler(msg, socket, requested));
 }
 
-function messageHandler(msg: any, socket: net.Socket) {
-  if (isHandshake(msg)) socket.write(message.buildInterested())
+function messageHandler(msg: any, socket: net.Socket, requested: any) {
+  if (isHandshake(msg)) {
+    socket.write(message.buildInterested())
+  } else {
+    const m = message.parse(msg);
+
+    if (m.id === 0) chokeHandler();
+    if (m.id === 1) unchokeHandler();
+    if (m.id === 4) haveHandler(m.payload, socket, requested);
+    if (m.id === 5) bitfieldHandler(m.payload);
+    if (m.id === 7) pieceHandler(m.payload);
+
+  }
 }
 
-function isHandshake(msg: any) {
+function chokeHandler() {  }
+
+function unchokeHandler() {  }
+
+function haveHandler(payload: Buffer, socket: net.Socket, requested: any) {
+  const pieceIndex = payload.readUInt32BE(0);
+
+  if (!requested[pieceIndex]) {
+    socket.write(message.buildRequest());
+  }
+
+  requested[pieceIndex] = true;
+}
+
+function bitfieldHandler(payload) {  }
+
+function pieceHandler(payload) {  }
+
+function isHandshake(msg: any): Boolean {
   return msg.length === msg.readUInt8(0) + 49 &&
     msg.toString('utf8', 1) === 'BitTorrent protocol';
 }
